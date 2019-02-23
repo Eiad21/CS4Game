2251e81bf0a0c91f694322ad75eb01be1d6561ce
Eiad21 <42401823+Eiad21@users.noreply.github.com>
2019-02-23 13:50:39 +0200

    Added Infection Class

---
+++ DisasterCity/src/model/disasters/Infection.java
@@ -0,0 +1,13 @@
+package model.disasters;
+
+import model.people.Citizen;
+import simulation.Rescuable;
+
+public class Infection extends Disaster {
+	
+	//Constructor(s)
+	public Infection(int cycle, Citizen target) {
+		super(cycle, target);
+	}
+
+}
