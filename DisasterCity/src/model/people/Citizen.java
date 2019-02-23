b17d8c6c37a6a508baf9352682cd5928b27254d4
Eiad21 <42401823+Eiad21@users.noreply.github.com>
2019-02-23 11:59:11 +0200

    Added missing imports and separated getters from setters

--- DisasterCity/src/model/people/Citizen.java
+++ DisasterCity/src/model/people/Citizen.java
@@ -1,73 +1,72 @@
 package model.people;
-import simulation.Rescuable;
-import simulation.Simulatable;
+import model.disasters.*;
+import simulation.*;
 public class Citizen {
-private CitizenState state; 
-private Disaster disaster;
-private Address location;
-private String nationalID;
-private String name;
-private int age;
-private int hp;
-private int bloodLoss;
-private int toxicity;
+	private CitizenState state; 
+	private Disaster disaster;
+	private Address location;
+	private String nationalID;
+	private String name;
+	private int age;
+	private int hp;
+	private int bloodLoss;
+	private int toxicity;
+	
 //Constructor
 public Citizen(Address location, String nationalID, String name, int age){
 	this.location=location;
 	this.nationalID=nationalID;
 	this.name=name;
 	this.age=age;
-	state=SAFE;
+	state=CitizenState.SAFE;
 }
-//Getters and Setters
-public CitizenState getState() {
-	return state;
-}
-public void setState(CitizenState state) {
-	this.state = state;
-}
-public Disaster getDisaster() {
-	return disaster;
-}
-
-public Address getLocation() {
-	return location;
-}
-public void setLocation(Address location) {
-	this.location = location;
-}
-public String getNationalID() {
-	return nationalID;
-}
-
-public String getName() {
-	return name;
-}
-
-public int getAge() {
-	return age;
-}
-
-public int getHp() {
-	return hp;
-}
-public void setHp(int hp) {
-	this.hp = hp;
-}
-public int getBloodLoss() {
-	return bloodLoss;
-}
-public void setBloodLoss(int bloodLoss) {
-	this.bloodLoss = bloodLoss;
-}
-public int getToxicity() {
-	return toxicity;
-}
-public void setToxicity(int toxicity) {
-	this.toxicity = toxicity;
-}
-
-
-
-
+	//Getter(s)
+	public CitizenState getState() {
+		return state;
+	}
+		
+	public Disaster getDisaster() {
+		return disaster;
+	}
+	
+	public Address getLocation() {
+		return location;
+	}
+	public String getNationalID() {
+		return nationalID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public int getBloodLoss() {
+		return bloodLoss;
+	}
+	public int getToxicity() {
+		return toxicity;
+	}
+	
+	//Setter(s)
+	public void setState(CitizenState state) {
+		this.state = state;
+	}
+	public void setLocation(Address location) {
+		this.location = location;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public void setBloodLoss(int bloodLoss) {
+		this.bloodLoss = bloodLoss;
+	}
+	public void setToxicity(int toxicity) {
+		this.toxicity = toxicity;
+	}
 }
