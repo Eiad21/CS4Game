ad8dfe56c859fbd5616b6ce9355a134664045dc2
Eiad21 <42401823+Eiad21@users.noreply.github.com>
2019-02-23 14:12:01 +0200

    Added Simulator class

---
+++ DisasterCity/src/simulation/Simulator.java
@@ -0,0 +1,23 @@
+package simulation;
+
+import java.util.ArrayList;
+
+import model.disasters.Disaster;
+import model.infrastucture.ResidentialBuilding;
+import model.people.Citizen;
+
+public class Simulator {
+	
+	//Instance Variables
+	private int currentCycle;
+	private ArrayList<ResidentialBuilding> buildings;
+	private ArrayList<Citizen> citizens;
+	private ArrayList<Disaster> plannedDisasters;
+	private ArrayList<Disaster> executedDisasters;
+	private Address[][] world;
+	
+	//Constructor(s)
+	public Simulator() {
+		//#TODO Implement body of constructor
+	}
+}
