a57bac6de6733f4b62c4fb392d3744f2c629805e
Eiad21 <42401823+Eiad21@users.noreply.github.com>
2019-02-28 18:25:07 +0200

    Formatting changes to Simulator

--- DisasterCity/src/simulation/Simulator.java
+++ DisasterCity/src/simulation/Simulator.java
@@ -24,7 +24,7 @@ public class Simulator {
 	// Static Variables
 	FileReader fileReader;
 	BufferedReader br;
-	
+
 	// Instance Variables
 	private int currentCycle;
 	private ArrayList<Unit> emergencyUnits;
@@ -34,7 +34,6 @@ public class Simulator {
 	private ArrayList<Disaster> executedDisasters;
 	private Address[][] world;
 
-	
 	public Simulator() throws Exception {
 		emergencyUnits = new ArrayList<>();
 		buildings = new ArrayList<ResidentialBuilding>();
@@ -42,10 +41,10 @@ public class Simulator {
 		plannedDisasters = new ArrayList<Disaster>();
 		executedDisasters = new ArrayList<Disaster>();
 		world = new Address[10][10];
-		 
+
 		// Initialization of world array
-		for(int i = 0;i<10;i++)
-			for(int j = 0;j<10;j++)
+		for (int i = 0; i < 10; i++)
+			for (int j = 0; j < 10; j++)
 				world[i][j] = new Address(i, j);
 		loadBuildings("buildings.csv");
 		loadCitizens("citizens.csv");
@@ -60,20 +59,25 @@ public class Simulator {
 		while ((currentLine = br.readLine()) != null) {
 			String[] a = currentLine.split(",");
 			Unit unit;
-			switch(a[0]) {
+			switch (a[0]) {
 			case "AMB":
-				unit = new Ambulance(a[1], world[0][0], Integer.parseInt(a[2]));break;
-			case"DCU":	
-				unit = new DiseaseControlUnit(a[1], world[0][0], Integer.parseInt(a[2]));break;
-			case"EVC":
-				unit = new Evacuator(a[1], world[0][0], Integer.parseInt(a[2]),Integer.parseInt(a[3]));break;
-			case"FTK":
-				unit = new FireTruck(a[1], world[0][0], Integer.parseInt(a[2]));break;
-			case"GCU":
-				unit = new GasControlUnit(a[1], world[0][0], Integer.parseInt(a[2]));break;	
+				unit = new Ambulance(a[1], world[0][0], Integer.parseInt(a[2]));
+				break;
+			case "DCU":
+				unit = new DiseaseControlUnit(a[1], world[0][0], Integer.parseInt(a[2]));
+				break;
+			case "EVC":
+				unit = new Evacuator(a[1], world[0][0], Integer.parseInt(a[2]), Integer.parseInt(a[3]));
+				break;
+			case "FTK":
+				unit = new FireTruck(a[1], world[0][0], Integer.parseInt(a[2]));
+				break;
+			case "GCU":
+				unit = new GasControlUnit(a[1], world[0][0], Integer.parseInt(a[2]));
+				break;
 			default:
 				unit = null;
-				
+
 			}
 			emergencyUnits.add(unit);
 		}
@@ -100,10 +104,10 @@ public class Simulator {
 			int citX = Integer.parseInt(a[0]);
 			int citY = Integer.parseInt(a[1]);
 			Address c = world[citX][citY];
-			Citizen citizen = new Citizen(c,a[2],a[3],Integer.parseInt(a[4]));
+			Citizen citizen = new Citizen(c, a[2], a[3], Integer.parseInt(a[4]));
 			citizens.add(citizen);
-			for(ResidentialBuilding b : buildings) {
-				if(b.getLocation().getX()==citX&&b.getLocation().getY()==citY)
+			for (ResidentialBuilding b : buildings) {
+				if (b.getLocation().getX() == citX && b.getLocation().getY() == citY)
 					b.addOccupant(citizen);
 			}
 		}
@@ -117,32 +121,38 @@ public class Simulator {
 			String[] a = currentLine.split(",");
 			int startCycle = Integer.parseInt(a[0]);
 			Disaster disaster;
-			switch(a[1]) {
+			switch (a[1]) {
 			case "FIR":
-				disaster = new Fire(startCycle, getBuilding(Integer.parseInt(a[2]),Integer.parseInt(a[2])));break;
-			case"GLK":	
-				disaster = new GasLeak(startCycle, getBuilding(Integer.parseInt(a[2]),Integer.parseInt(a[2])));break;
-			case"INJ":
-				disaster = new Injury(startCycle, getCitizen(a[2]));break;
-			case"INF":
-				disaster = new Infection(startCycle, getCitizen(a[2]));break;
+				disaster = new Fire(startCycle, getBuilding(Integer.parseInt(a[2]), Integer.parseInt(a[2])));
+				break;
+			case "GLK":
+				disaster = new GasLeak(startCycle, getBuilding(Integer.parseInt(a[2]), Integer.parseInt(a[2])));
+				break;
+			case "INJ":
+				disaster = new Injury(startCycle, getCitizen(a[2]));
+				break;
+			case "INF":
+				disaster = new Infection(startCycle, getCitizen(a[2]));
+				break;
 			default:
 				disaster = null;
-				
+
 			}
 			plannedDisasters.add(disaster);
 		}
 	}
+
 	private ResidentialBuilding getBuilding(int x, int y) {
-		for(ResidentialBuilding building : buildings) {
-			if(building.getLocation().getX()==x&&building.getLocation().getY()==y)
+		for (ResidentialBuilding building : buildings) {
+			if (building.getLocation().getX() == x && building.getLocation().getY() == y)
 				return building;
 		}
 		return null;
 	}
+
 	private Citizen getCitizen(String nationalID) {
-		for(Citizen citizen : citizens) {
-			if(citizen.getNationalID().equals(nationalID))
+		for (Citizen citizen : citizens) {
+			if (citizen.getNationalID().equals(nationalID))
 				return citizen;
 		}
 		return null;
